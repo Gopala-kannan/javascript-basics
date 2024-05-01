0e78ba80407666289c94bdaaf998eac3514e1abf
Gopala-kannan <gopalakannanekannan@gmail.com>
2024-05-01 20:25:25 +0530

    javascript

--- inner1.js
+++
@@ -1,8 +0,0 @@
-var fruit = ["Apple","orange","Grape","Banana"]
-
-console.log(fruit.length)
-for(count=0;count<=5;count=count+1)
-{
-    console.log(fruit[count])
-}
-
---
+++ javascript-basic-code/inner1.js
@@ -0,0 +1,8 @@
+var fruit = ["Apple","orange","Grape","Banana"]
+
+console.log(fruit.length)
+for(count=0;count<=5;count=count+1)
+{
+    console.log(fruit[count])
+}
+
